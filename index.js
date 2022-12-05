2603b169a4896bab64eebd11f29c9bd2f264fd60
hayacaw <hayacaw@gmail.com>
2022-12-06 00:37:00 +0900

    レスポンスから不要な文字列を削除

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
   });
-  res.end(`echo ${path}`);
+  res.end(${path});
 });
 const port = 8000;
 server.listen(port, () => {
